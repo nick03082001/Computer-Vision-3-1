b130c76039c092af8a468ba60c9f08e99dd458f1
Nick03082001 <74454227+nick03082001@users.noreply.github.com>
2022-02-26 08:58:49 +0700

    Add files via upload

---
+++ Historgram/ADAPTIVE_THRESHOLD_TYPE.cs
@@ -0,0 +1,10 @@
+﻿using Emgu.CV.CvEnum;
+
+namespace Historgram
+{
+    internal class ADAPTIVE_THRESHOLD_TYPE
+    {
+        public static AdaptiveThresholdType CV_ADAPTIVE_THRESH_MEAN_C { get; internal set; }
+        public static AdaptiveThresholdType CV_ADAPTIVE_THRESH_GAUSSIAN_C { get; internal set; }
+    }
+}
