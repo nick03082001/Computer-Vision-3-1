7f3cef4f717fa152b3e021623f1724c979d6c694
Nick03082001 <74454227+nick03082001@users.noreply.github.com>
2022-02-26 08:45:39 +0700

    Add files via upload

---
+++ ນ ໃຈປະສົງ ວົງພັນສີ 3CS2 (ອາທິດທີ 2-3) Black and White/ນ ໃຈປະສົງ ວົງພັນສີ 3CS2 (ອາທິດທີ 2)/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace ນ_ໃຈປະສົງ_ວົງພັນສີ_3CS2__ອາທິດທີ_2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOriginal = new System.Windows.Forms.Button();
+            this.btnGrayScale1 = new System.Windows.Forms.Button();
+            this.btnGrayScale2 = new System.Windows.Forms.Button();
+            this.btnBlack_White_1 = new System.Windows.Forms.Button();
+            this.btnNagative = new System.Windows.Forms.Button();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.lblThresold = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_OpenPicture = new System.Windows.Forms.Button();
+            this.btn_Log_Transformation = new System.Windows.Forms.Button();
+            this.lblC_LogTransformation = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblC_PowerLaw = new System.Windows.Forms.Button();
+            this.lblcPowerLaw = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblAlpha = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOriginal
+            // 
+            this.btnOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnOriginal.Location = new System.Drawing.Point(52, 12);
+            this.btnOriginal.Name = "btnOriginal";
+            this.btnOriginal.Size = new System.Drawing.Size(113, 33);
+            this.btnOriginal.TabIndex = 0;
+            this.btnOriginal.Text = "Original";
+            this.btnOriginal.UseVisualStyleBackColor = true;
+            this.btnOriginal.Click += new System.EventHandler(this.btnOriginal_Click);
+            // 
+            // btnGrayScale1
+            // 
+            this.btnGrayScale1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnGrayScale1.Location = new System.Drawing.Point(220, 12);
+            this.btnGrayScale1.Name = "btnGrayScale1";
+            this.btnGrayScale1.Size = new System.Drawing.Size(124, 33);
+            this.btnGrayScale1.TabIndex = 1;
+            this.btnGrayScale1.Text = "Gray Scale 1";
+            this.btnGrayScale1.UseVisualStyleBackColor = true;
+            this.btnGrayScale1.Click += new System.EventHandler(this.btnGrayScale1_Click);
+            // 
+            // btnGrayScale2
+            // 
+            this.btnGrayScale2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnGrayScale2.Location = new System.Drawing.Point(403, 12);
+            this.btnGrayScale2.Name = "btnGrayScale2";
+            this.btnGrayScale2.Size = new System.Drawing.Size(121, 33);
+            this.btnGrayScale2.TabIndex = 2;
+            this.btnGrayScale2.Text = "Gray Scale 2";
+            this.btnGrayScale2.UseVisualStyleBackColor = true;
+            this.btnGrayScale2.Click += new System.EventHandler(this.btnGrayScale2_Click);
+            // 
+            // btnBlack_White_1
+            // 
+            this.btnBlack_White_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnBlack_White_1.Location = new System.Drawing.Point(594, 98);
+            this.btnBlack_White_1.Name = "btnBlack_White_1";
+            this.btnBlack_White_1.Size = new System.Drawing.Size(163, 35);
+            this.btnBlack_White_1.TabIndex = 3;
+            this.btnBlack_White_1.Text = "Black_White 1";
+            this.btnBlack_White_1.UseVisualStyleBackColor = true;
+            this.btnBlack_White_1.Click += new System.EventHandler(this.btnBlack_White_1_Click);
+            // 
+            // btnNagative
+            // 
+            this.btnNagative.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnNagative.Location = new System.Drawing.Point(594, 185);
+            this.btnNagative.Name = "btnNagative";
+            this.btnNagative.Size = new System.Drawing.Size(163, 33);
+            this.btnNagative.TabIndex = 4;
+            this.btnNagative.Text = "Negative";
+            this.btnNagative.UseVisualStyleBackColor = true;
+            this.btnNagative.Click += new System.EventHandler(this.btnNagative_Click);
+            // 
+            // btnInvert
+            // 
+            this.btnInvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnInvert.Location = new System.Drawing.Point(594, 224);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(163, 32);
+            this.btnInvert.TabIndex = 5;
+            this.btnInvert.Text = "Invert";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+            // 
+            // lblThresold
+            // 
+            this.lblThresold.AutoSize = true;
+            this.lblThresold.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblThresold.Location = new System.Drawing.Point(591, 158);
+            this.lblThresold.Name = "lblThresold";
+            this.lblThresold.Size = new System.Drawing.Size(66, 18);
+            this.lblThresold.TabIndex = 6;
+            this.lblThresold.Text = "Thresold";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(663, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 24);
+            this.textBox1.TabIndex = 7;
+            // 
+            // btn_OpenPicture
+            // 
+            this.btn_OpenPicture.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btn_OpenPicture.Location = new System.Drawing.Point(594, 12);
+            this.btn_OpenPicture.Name = "btn_OpenPicture";
+            this.btn_OpenPicture.Size = new System.Drawing.Size(163, 33);
+            this.btn_OpenPicture.TabIndex = 9;
+            this.btn_OpenPicture.Text = "Open Picture";
+            this.btn_OpenPicture.UseVisualStyleBackColor = true;
+            this.btn_OpenPicture.Click += new System.EventHandler(this.btn_OpenPicture_Click);
+            // 
+            // btn_Log_Transformation
+            // 
+            this.btn_Log_Transformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btn_Log_Transformation.Location = new System.Drawing.Point(594, 262);
+            this.btn_Log_Transformation.Name = "btn_Log_Transformation";
+            this.btn_Log_Transformation.Size = new System.Drawing.Size(163, 32);
+            this.btn_Log_Transformation.TabIndex = 10;
+            this.btn_Log_Transformation.Text = "Log Transformation";
+            this.btn_Log_Transformation.UseVisualStyleBackColor = true;
+            this.btn_Log_Transformation.Click += new System.EventHandler(this.btn_Log_Transformation_Click);
+            // 
+            // lblC_LogTransformation
+            // 
+            this.lblC_LogTransformation.AutoSize = true;
+            this.lblC_LogTransformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblC_LogTransformation.Location = new System.Drawing.Point(611, 303);
+            this.lblC_LogTransformation.Name = "lblC_LogTransformation";
+            this.lblC_LogTransformation.Size = new System.Drawing.Size(32, 18);
+            this.lblC_LogTransformation.TabIndex = 11;
+            this.lblC_LogTransformation.Text = "C =";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(649, 300);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 24);
+            this.textBox2.TabIndex = 12;
+            // 
+            // lblC_PowerLaw
+            // 
+            this.lblC_PowerLaw.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblC_PowerLaw.Location = new System.Drawing.Point(594, 330);
+            this.lblC_PowerLaw.Name = "lblC_PowerLaw";
+            this.lblC_PowerLaw.Size = new System.Drawing.Size(163, 32);
+            this.lblC_PowerLaw.TabIndex = 13;
+            this.lblC_PowerLaw.Text = "Power Law";
+            this.lblC_PowerLaw.UseVisualStyleBackColor = true;
+            this.lblC_PowerLaw.Click += new System.EventHandler(this.lblC_PowerLaw_Click);
+            // 
+            // lblcPowerLaw
+            // 
+            this.lblcPowerLaw.AutoSize = true;
+            this.lblcPowerLaw.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblcPowerLaw.Location = new System.Drawing.Point(611, 365);
+            this.lblcPowerLaw.Name = "lblcPowerLaw";
+            this.lblcPowerLaw.Size = new System.Drawing.Size(32, 18);
+            this.lblcPowerLaw.TabIndex = 14;
+            this.lblcPowerLaw.Text = "C =";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox3.Location = new System.Drawing.Point(649, 368);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 24);
+            this.textBox3.TabIndex = 15;
+            // 
+            // lblAlpha
+            // 
+            this.lblAlpha.AutoSize = true;
+            this.lblAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblAlpha.Location = new System.Drawing.Point(596, 406);
+            this.lblAlpha.Name = "lblAlpha";
+            this.lblAlpha.Size = new System.Drawing.Size(61, 18);
+            this.lblAlpha.TabIndex = 16;
+            this.lblAlpha.Text = "Alpha = ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox4.Location = new System.Drawing.Point(663, 403);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(94, 24);
+            this.textBox4.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(541, 374);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.lblAlpha);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblcPowerLaw);
+            this.Controls.Add(this.lblC_PowerLaw);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblC_LogTransformation);
+            this.Controls.Add(this.btn_Log_Transformation);
+            this.Controls.Add(this.btn_OpenPicture);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblThresold);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.btnNagative);
+            this.Controls.Add(this.btnBlack_White_1);
+            this.Controls.Add(this.btnGrayScale2);
+            this.Controls.Add(this.btnGrayScale1);
+            this.Controls.Add(this.btnOriginal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOriginal;
+        private System.Windows.Forms.Button btnGrayScale1;
+        private System.Windows.Forms.Button btnGrayScale2;
+        private System.Windows.Forms.Button btnBlack_White_1;
+        private System.Windows.Forms.Button btnNagative;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.Label lblThresold;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_OpenPicture;
+        private System.Windows.Forms.Button btn_Log_Transformation;
+        private System.Windows.Forms.Label lblC_LogTransformation;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button lblC_PowerLaw;
+        private System.Windows.Forms.Label lblcPowerLaw;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblAlpha;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ນ ໃຈປະສົງ ວົງພັນສີ 3CS2 (ອາທິດທີ 2-3) Black and White/ນ ໃຈປະສົງ ວົງພັນສີ 3CS2 (ອາທິດທີ 2)/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ນ_ໃຈປະສົງ_ວົງພັນສີ_3CS2__ອາທິດທີ_2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            Bitmap pict_C;
+            Bitmap pict_O;
+           // Bitmap pict_O = (Bitmap)Image.FromFile("D:\\comvision\\Image_comvision\\dog.jpg");
+
+            public Bitmap ConvertToGrayScale(Bitmap source)
+            {
+                Bitmap bmp = new Bitmap(source.Width, source.Height);
+                for (int i = 0; i < source.Width; i++)
+                {
+                    for (int j = 0; j < source.Height; j++)
+                    {
+                        Color c = source.GetPixel(i, j);
+                        int avg = (int)((c.R + c.G + c.B) / 3);
+                        bmp.SetPixel(i, j, Color.FromArgb(avg, avg, avg));
+                    }
+                }
+                return bmp;
+            }
+
+            public Bitmap ConvertToGrayScale2(Bitmap source)
+            {
+                Bitmap bmp = new Bitmap(source.Width, source.Height);
+                for (int i = 0; i < source.Width; i++)
+                {
+                    for (int j = 0; j < source.Height; j++)
+                    {
+                        Color c = source.GetPixel(i, j);
+                        //Luminance
+                        int nP = (int)(0.3 * c.R + 0.59 * c.G + 0.11 * c.B);
+                        bmp.SetPixel(i, j, Color.FromArgb(nP, nP, nP));
+                    }
+                }
+                return bmp;
+            }
+            //setting threshold
+            public Bitmap Thresholding1(Bitmap source)
+            {
+                Bitmap bmp = new Bitmap(source.Width, source.Height);
+                int t = int.Parse(textBox1.Text);
+
+                for (int i = 0; i < source.Width; i++)
+                {
+                    for (int j = 0; j < source.Height; j++)
+                    {
+                        Color c = source.GetPixel(i, j);
+                        int avg = (int)((c.R + c.G + c.B) / 3);
+                        if (avg > t)
+                            avg = 255;
+                        else avg = 0;
+
+                        bmp.SetPixel(i, j, Color.FromArgb(avg, avg, avg));
+                    }
+                }
+                return bmp;
+            }
+            //setting invert
+            public Bitmap Invert(Bitmap source)
+            {
+                Bitmap bmp = new Bitmap(source.Width, source.Height);
+                int t = int.Parse(textBox1.Text);
+
+                for (int i = 0; i < source.Width; i++)
+                {
+                    for (int j = 0; j < source.Height; j++)
+                    {
+                        Color c = source.GetPixel(i, j);
+                        int avg = (int)((c.R + c.G + c.B) / 3);
+                        if (avg == 255)
+                            avg = 0;
+                        else if (avg == 0)
+                            avg = 255;
+
+                        bmp.SetPixel(i, j, Color.FromArgb(avg, avg, avg));
+                    }
+                }
+                return bmp;
+            }
+        //setting negative
+        public Bitmap Negative(Bitmap source)
+        {
+            Bitmap bmp = new Bitmap(source.Width, source.Height);
+            int t = int.Parse(textBox1.Text);
+
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color c = source.GetPixel(i, j);
+                    int r1, g1, b1;
+                    r1 = 255 - (int)(c.R);
+                    g1 = 255 - (int)(c.G);
+                    b1 = 255 - (int)(c.B);
+                    bmp.SetPixel(i, j, Color.FromArgb(r1, g1, b1));
+                }
+            }
+            return bmp;
+        }
+        //original
+        private void btnOriginal_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pict_O;
+        }
+        //grayscale1
+        private void btnGrayScale1_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            pictureBox1.Image = ConvertToGrayScale(pict_C);
+        }
+        //grayscale2
+        private void btnGrayScale2_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            pictureBox1.Image = ConvertToGrayScale2(pict_C);
+        }
+        //black and white1
+        private void btnBlack_White_1_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            pictureBox1.Image = Thresholding1(pict_C);
+        }
+        //negative
+        private void btnNagative_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            pictureBox1.Image = Negative(pict_C);
+        }
+        //invert
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            pictureBox1.Image = Invert(pict_C);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pict_O;
+            pictureBox1.Refresh();
+
+        }
+
+        private void btn_OpenPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Title = "Open image from path";
+            opf.Filter = "Png Images (*.png)|*.png|Jpeg Images (*.jpg)|*.jpg|Bitmap Images (*bmp)|*.bmp";
+            if (opf.ShowDialog()==DialogResult.OK)
+            {
+                pict_O = new Bitmap(opf.FileName);
+                pict_C = pict_O;
+                pictureBox1.Image = pict_C;
+            }
+
+        }
+        public Bitmap logTransformation(Bitmap source)
+        {
+            Bitmap log = new Bitmap(source.Width, source.Height);
+            int C = int.Parse(textBox2.Text);
+
+            for (int y = 0; y < source.Height; y++)
+            {
+                for (int x = 0; x < source.Width; x++)
+                {
+                    Color n = source.GetPixel(x, y);
+                    int sR = (int)(C * Math.Log10(1 + n.R));
+                    int sG = (int)(C * Math.Log10(1 + n.G));
+                    int sB = (int)(C * Math.Log10(1 + n.B));
+                    log.SetPixel(x, y, Color.FromArgb(n.A, sR, sG, sB));
+                }
+            }
+            return log;
+        }
+            public Bitmap powerLaw (Bitmap source, double R)
+            {
+            Bitmap bmp = new Bitmap(source.Width, source.Height);
+            int c = int.Parse(textBox3.Text.ToString().Trim());
+
+            for (int y = 0; y < source.Height; y++)
+            {
+                for (int x = 0; x < source.Width; x++)
+                {
+                    Color renk = source.GetPixel(x, y);
+                    double gd = ((double)R / 20);
+                    int sR = (int)(c * Math.Pow((renk.R / 255.0), gd));
+                    int sG = (int)(c * Math.Pow((renk.G / 255.0), gd));
+                    int sB = (int)(c * Math.Pow((renk.B / 255.0), gd));
+                    bmp.SetPixel(x, y, Color.FromArgb(renk.A, sR, sG, sB));
+                }
+            }
+            return bmp;
+        }
+
+        private void btn_Log_Transformation_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            pictureBox1.Image = logTransformation(pict_C);
+        }
+
+        private void lblC_PowerLaw_Click(object sender, EventArgs e)
+        {
+            pict_C = new Bitmap(pictureBox1.Image);
+            //textBox4.Text = (int.Parse(textBox3.Text.ToString()) / 20.0).ToString("F2");
+            pictureBox1.Image = powerLaw(pict_C, double.Parse(textBox4.Text.ToString()));
+        }
+    }
+
+
+
+ 
+}
